11ea1e4850c4537b75e4b1f7a728ec79134dae3d
szxex <masato_miyai@hotmail.com>
2026-05-13 10:24:52 +0900

    Update utils.py

--- tagger/utils.py
+++ tagger/utils.py
@@ -1,12 +1,13 @@
 import os
 
+
 from typing import List, Dict
 from pathlib import Path
 
 from modules import shared, scripts
 from preload import default_ddp_path
 from tagger.preset import Preset
-from tagger.interrogator import Interrogator, DeepDanbooruInterrogator, WaifuDiffusionInterrogator, TransformerInterrogator, PixAIInterrogator, GeneralInterrogator
+from tagger.interrogator import Interrogator, DeepDanbooruInterrogator, WaifuDiffusionInterrogator, OracleInterrogator, PixAIInterrogator,CamieInterrogator, GeneralInterrogator
 
 preset = Preset(Path(scripts.basedir(), 'presets'))
 
@@ -47,22 +48,44 @@ def refresh_interrogators() -> List[str]:
         'pixai-tagger-v0.9': PixAIInterrogator(
             'pixai-tagger-v0.9',
              repo_id='deepghs/pixai-tagger-v0.9-onnx',
+             #character_thresholds=0.6
+             #defalt character_thresholds=None *character_thresholds=0
         ),
         'pixai-tagger-v0.9E': GeneralInterrogator(
             'pixai-tagger-v0.9E',
              repo_id='etset/pixai-tagger-v0.9E',
              model_path='pixai-tagger-v0.9.onnx',
              tags_path='pixai-tagger-v0.9.csv',
-             key_name='output',
         ),
-        'OppaiOracle-v1.1': TransformerInterrogator(
+        'OppaiOracle-v1.1': OracleInterrogator(
             'OppaiOracle-v1.1',
              model_path='V1.1_onnx/model.onnx',
              tags_path='V1.1_onnx/selected_tags.csv',
              preproc_path='V1.1_onnx/preprocessing.json',
              repo_id='Grio43/OppaiOracle',
-             
         ),
+        'camie-tagger-v2': CamieInterrogator(
+            'camie-tagger-v2',
+             model_path='camie-tagger-v2.onnx',
+             metadata_path='camie-tagger-v2-metadata.json',
+             repo_id='Camais03/camie-tagger-v2',
+             #fmt=('artist','character','copyright','general','meta','rating','year') 
+             #default fmt = ('character','general','rating') 
+             #category_thresholds={"artist":0.5,"character":0.8,"copyright":0.7,"general":0.3,"meta":0.2,"rating":0.5,"year":0.2} 
+             #default category_thresholds=None *same thresholds
+        ),
+        #'tagger-name': GeneralInterrogator(
+        #    'tagger-name',
+        #     repo_id='repo_id',
+        #     model_path='model.onnx',
+        #     tags_path='selected_tags.csv',
+        #     doStd=True,
+        #      *defalut is True,normarize 0-1
+        #     normalize={"mean":[0.5,0.5,0.5],"std":[0.5,0.5,0.5]}, 
+        #      *If you don't want to normalize the data, specify `None`. The values may be listed in the model description, `preprocess.json`, `config.json`, or similar files.
+        #     rgb="RGB",
+        #      *default is "RGB",If the results look strange, setting it to “BGR” might fix the problem.
+        #),
     }
 
     # load deepdanbooru project
@@ -83,5 +106,7 @@ def refresh_interrogators() -> List[str]:
         interrogators[path.name] = DeepDanbooruInterrogator(path.name, path)
 
     return sorted(interrogators.keys())
+
+
 def split_str(s: str, separator=',') -> List[str]:
     return [x.strip() for x in s.split(separator) if x]
